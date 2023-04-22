16d0d370c65d864501446e3aeab45ef2cdfe1c28
Dolly62 <131373640+Dolly62@users.noreply.github.com>
2023-04-22 16:21:07 +0530

    addExistFile
    using querySelector and querySelectorAll

--- dom.js
+++ dom.js
@@ -1,19 +1,22 @@
-// 
-/* var header = document.getElementById('main-header');
-header.style.border = 'solid  3px #000';
 
-var items = document.querySelector('.title');
+
+// var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+// secondItem.style.backgroundColor = 'green';
+
+// var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+// thirdItem.style.display = 'none';
+
+
+var items = document.querySelectorAll('.list-group-item');
 console.log(items);
-items.textContent = 'ADD ITEMS';
-items.style.fontWeight = 'bold';
-items.style.color = 'green'; */
+items[1].style.color = 'green';
 
-var items = document.getElementsByClassName('list-group-item');
 
-// 3rd element with green background
-items[3].style.backgroundColor = 'green';
-
-// all elements are bold
-for(var i = 0; i < items.length; i++){
-    items[i].style.fontWeight = 'bold';
+var odd = document.querySelectorAll('.list-group-item:nth-child(odd)');
+for(var i = 0; i < odd.length; i++) {
+    odd[i].style.backgroundColor = 'green';
 }
+
+
+
+
