2a3bdce0fbb6b6047cb7fb553652d718aebda5ab
Dolly62 <131373640+Dolly62@users.noreply.github.com>
2023-04-22 13:52:46 +0530

    addDomfile

---
+++ dom.js
@@ -0,0 +1,9 @@
+// 
+var header = document.getElementById('main-header');
+header.style.border = 'solid  3px #000';
+
+var items = document.querySelector('.title');
+console.log(items);
+items.textContent = 'ADD ITEMS';
+items.style.fontWeight = 'bold';
+items.style.color = 'green';
