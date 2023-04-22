a40a534085c42756bc5dadada4f22a699b844d31
Dolly62 <131373640+Dolly62@users.noreply.github.com>
2023-04-22 15:15:29 +0530

    item3's background color change

--- dom.js
+++ dom.js
@@ -1,9 +1,19 @@
 // 
-var header = document.getElementById('main-header');
+/* var header = document.getElementById('main-header');
 header.style.border = 'solid  3px #000';
 
 var items = document.querySelector('.title');
 console.log(items);
 items.textContent = 'ADD ITEMS';
 items.style.fontWeight = 'bold';
-items.style.color = 'green';
+items.style.color = 'green'; */
+
+var items = document.getElementsByClassName('list-group-item');
+
+// 3rd element with green background
+items[3].style.backgroundColor = 'green';
+
+// all elements are bold
+for(var i = 0; i < items.length; i++){
+    items[i].style.fontWeight = 'bold';
+}
