7399cc4bbb9cfb06aa130fe39cdafc41be37d758
Dolly62 <kambojdolly798@gmail.com>
2023-04-24 19:11:29 +0530

    Save Multiple User Data

--- formLocsto.js
+++ formLocsto.js
@@ -1,14 +1,27 @@
-function test() {
-    const currentName = document.getElementById('username').value;
+function saveUserInput(event) {
+    event.preventDefault();
+    const currentName =document.getElementById('username').value;
     const currentEmail = document.getElementById('e-mail').value;
-    const currentPhoneNumber = document.getElementById('phone-number').value;
+    const currentPhoneNumber =document.getElementById('phone-number').value;
+    const userDetail = {
+        currentName,
+        currentEmail,
+        currentPhoneNumber
+
+    }
 
     //store the user data
-    const userInput = localStorage.setItem('currentName', currentName);
-    const userEmail = localStorage.setItem('currentEmail', currentEmail);
-    const userPhoneNumber = localStorage.setItem('currentPhoneNumber', currentPhoneNumber);
-
+    // const userInput = localStorage.setItem('currentName', currentName);
+    // const userEmail = localStorage.setItem('currentEmail', currentEmail);
+    // const userPhoneNumber = localStorage.setItem('currentPhoneNumber', currentPhoneNumber);
+    localStorage.setItem('userInfo', JSON.stringify(userDetail));
+    showUserOnScreen(userDetail)
 }
+function showUserOnScreen(userDetail) {
+    const parentitem = document.getElementById('listOfItems');
+    parentitem.innerHTML = parentitem.innerHTML + `<li>${userDetail.currentName} - ${userDetail.currentEmail} - ${userDetail.currentPhoneNumber}</li>`
+}
+
 
 
 
